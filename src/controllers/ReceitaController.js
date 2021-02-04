70a9bdb2cd6b975863eae7b00966fe3e3c5daccc
Joao Pedro <joaoplay16@gmail.com>
2021-02-04 20:33:27 -0300

    Api pronta

--- src/controllers/ReceitaController.js
+++ src/controllers/ReceitaController.js
@@ -10,7 +10,11 @@ module.exports={
     //metodo listar
     async index(req, res){
         const{page} = req.query;
-        const despesa = await Receita.paginate({}, {page, limit: 500});
+        const despesa = await Receita.paginate({}, {page, limit: 500}).then(d => {
+            console.log('DADOS', d);
+        }).catch(e => {
+            console.log('DEU ERRO', e);
+        })
         return res.json(despesa);
     },
     //metodo de detalhes
